cf50f54a19f510f2754a4c344449f5cba3b12294
Assessment 2 - Platformer <jamesmillard@outlook.com>
2015-07-06 19:38:40 +1000

    Final V6
    v.6

---
+++ game.js
@@ -0,0 +1,66 @@
+function Game(canvas) {
+  var self = this
+
+  this.context = canvas.getContext("2d")
+  this.width = canvas.width
+  this.height = canvas.height
+
+  // Keep track of key states
+  // Eg.:
+  //   game.keyPressed.up === true  // while UP key is pressed)
+  //   game.keyPressed.up === false // when UP key is released)
+  this.keyPressed = {}
+
+  $(canvas).on('keydown keyup', function(e) {
+    // Convert key code to key name
+    var keyName = Game.keys[e.which]
+
+    if (keyName) {
+      // eg.: `self.keyPressed.up = true` on keydown
+      // Will be set to `false` on keyup
+      self.keyPressed[keyName] = e.type === 'keydown'
+      e.preventDefault()
+    }
+  })
+}
+
+
+Game.keys = {
+
+	32: 'space',
+	37: 'left',
+	81: 'up',
+	39: 'right',
+	65: 'down'
+}
+
+Game.prototype.start = function(){
+	var self = this,
+	fps = 60,
+	interval = 1000/fps;
+
+	setInterval(function(){
+		self.update();
+		self.draw();
+	},interval);
+
+}
+
+Game.prototype.update = function(){
+	var self = this;
+	this.entities.forEach(function(entity){
+		if(entity.update) entity.update();
+
+	});
+
+}
+
+Game.prototype.draw = function(){
+	var self = this;
+	this.entities.forEach(function(entity){
+		if(entity.draw) entity.draw(self.context);
+	})
+
+
+}
+
